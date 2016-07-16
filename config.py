ad901a164083aa102e9dfd259c5f433582480c8d
linxunke <linxunke1@gmail.com>
2016-07-16 22:55:53 +0100

    11
    2

--- config.py
+++ config.py
@@ -8,8 +8,13 @@ OPENID_PROVIDERS = [
 
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' }
 ]
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
 MAX_SEARCH_RESULTS = 50
